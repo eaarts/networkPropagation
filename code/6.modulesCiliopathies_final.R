a2ffc2525545444366b7674c70c537c353a86842
eaarts <86360960+eaarts@users.noreply.github.com>
2024-12-10 17:00:34 +0100

    clean code

--- code/6.modulesCiliopathies_final.R
+++ code/6.modulesCiliopathies_final.R
@@ -1,27 +1,26 @@
 # clusters related to ciliopathies
 
-# Set working directory ----
-
-setwd('W:/GROUP/Users/Ellen/NetworkPropagation/')
-
 # Libraries ---- 
 
 library(tidyverse)
 library(ComplexHeatmap)
 library(doParallel)
 library(circlize)
+library(igraph)
+library(ggraph)
+library(clusterProfiler)
 
 '%notin%' = Negate('%in%')
 
 # Load files ----
 
-PPIClusters = read.csv('20230929_PPIFullNetworkClusters_allEdges_max20Nodes.csv') #clusters
+PPIClusters = read.csv('data/PPIFullNetworkClusters.csv') #clusters
 
-pageRankScores = readRDS('20231206_pageRankScores_JBTSnoOverlap.rds') #calculated network propagation scores for ciliopathies
+pageRankScores = readRDS('data/pagerankScores.rds') #calculated network propagation scores for ciliopathies
 
-traitAnnotation = read.csv('20231206_traitOverview_JBTSnoOverlap.csv') #labels for ciliopathies
+traitAnnotation = read.csv('data/traitOverview.csv') #labels for ciliopathies
 
-variantsCiliopathy = read.csv('20231206_variantsCiliopathy_JBTSnoOverlap.csv') #curated list of ciliopathy genes
+variantsCiliopathy = read.csv('data/variantsCiliopathies.csv') #curated list of ciliopathy genes
 
 # cilia page rank scores ----
 
@@ -80,54 +79,6 @@ for (i in 1:nrow(wilcoxRes_binary)) {
 
 colnames(wilcoxRes_variable) = traitAnnotation$trait_label[match(colnames(wilcoxRes_variable), traitAnnotation$Var1)]
 
-
-Heatmap(
-  wilcoxRes_variable,
-  col = colorRamp2(c(0, 3), c("white", "red")),
-  rect_gp = gpar(col = "white", lwd = 2),
-  cluster_columns = hclustTraits,
-  cell_fun = function(j, i, x, y, w, h, fill) {
-    if (wilcoxRes_variable[i, j] > 3) {
-      grid.text("***", x, y)
-    } else if (wilcoxRes_variable[i, j] > 2) {
-      grid.text("**", x, y) 
-    } else if (wilcoxRes_variable[i, j] > 1.3) {
-      grid.text("*", x, y)
-    }
-    
-  }
-)
-
-Heatmap(
-  wilcoxRes_variable,
-  col = colorRamp2(c(0, 3), c("white", "red")),
-  rect_gp = gpar(col = "white", lwd = 2),
-  cluster_columns = hclustTraits,
-  cell_fun = function(j, i, x, y, w, h, fill) {
-    if (wilcoxRes_binary[i, j] == 1) {
-      grid.text("*", x, y)
-    }
-  }
-)
-
-
-Heatmap(
-  t(wilcoxRes_variable),
-  col = colorRamp2(c(0, 3), c("white", "red")),
-  rect_gp = gpar(col = "white", lwd = 2),
-  cluster_rows = hclustTraits,
-  cell_fun = function(j, i, x, y, w, h, fill) {
-    if (wilcoxRes_variable[j, i] > 3) {
-      grid.text("***", x, y)
-    } else if (wilcoxRes_variable[j, i] > 2) {
-      grid.text("**", x, y) 
-    } else if (wilcoxRes_variable[j, i] > 1.3) {
-      grid.text("*", x, y)
-    }
-    
-  }
-)
-
 # only take modules with known variant
 wilcoxRes_binary = wilcoxRes_adjusted
 for (i in 1:nrow(wilcoxRes_binary)) {
@@ -149,53 +100,50 @@ colnames(wilcoxRes_binary) = traitAnnotation$trait_label[match(colnames(wilcoxRe
 
 Heatmap(wilcoxRes_binary, cluster_columns = hclustTraits, col = c('white', 'black'), rect_gp = gpar(col = "white", lwd = 2))
 
-# enrich per phenotype ----
+# cluster protein modules using re-clustering pattern ----
 
-phenotype = data.frame('ciliopathy' = unique(traitAnnotation[traitAnnotation$ciliopathy == T, "trait_label"]), 'renal' = F, 'retina' = F, 'CNS' = F, 'polydactyly' = F, 'skeletal' = F, 'hearing' = F)
-phenotype$renal[grep('kidney|meckel|renal|nephro|senior|orofacio|bardet', phenotype$ciliopathy, ignore.case = T)] = T
-phenotype$retina[grep('ocular|bardet|senior|usher|leber|retinitis|cone', phenotype$ciliopathy, ignore.case = T)] = T
-phenotype$CNS[grep('joubert|meckel|acrocallosal|hydrolethalus|orofacio|bardet', phenotype$ciliopathy, ignore.case = T)] = T
-phenotype$polydactyly[grep('meckel|orofacio|acrocallosal|bardet|hydrolethalus|ellis|short rib', phenotype$ciliopathy, ignore.case = T)] = T
-phenotype$skeletal[grep('Jeune|short rib|ellis|cranioe', phenotype$ciliopathy, ignore.case = T)] = T
-phenotype$hearing[grep('usher', phenotype$ciliopathy, ignore.case = T)] = T
+clusteringDF = PPIClusters[,"walktrapCluster", drop =F]
 
-wilcoxRes_adjusted_long = wilcoxRes_binary %>% as.data.frame() %>% rownames_to_column(var = 'complex') %>% gather(key = 'ciliopathy', value = 'sign', -complex)
-wilcoxRes_adjusted_long = merge(wilcoxRes_adjusted_long, phenotype, by = 'ciliopathy')
-wilcoxRes_adjusted_long_sig = wilcoxRes_adjusted_long[wilcoxRes_adjusted_long$sign == T,]
+clusteringDF$clust1 = 1
+clusteringDF$clust2 = gsub(';.*','',clusteringDF$walktrapCluster)
+clusteringDF$clust3 = sapply(strsplit(clusteringDF$walktrapCluster,';'), FUN = function(x) paste(x[1:2], collapse = ';'))
+clusteringDF$clust4 = sapply(strsplit(clusteringDF$walktrapCluster,';'), FUN = function(x) paste(x[1:3], collapse = ';'))
+clusteringDF$clust5 = sapply(strsplit(clusteringDF$walktrapCluster,';'), FUN = function(x) paste(x[1:4], collapse = ';'))
+clusteringDF$clust6 = sapply(strsplit(clusteringDF$walktrapCluster,';'), FUN = function(x) paste(x[1:5], collapse = ';'))
 
-enrichComplex = unique(wilcoxRes_adjusted_long[, c("complex"), drop = F])
-for (j in colnames(phenotype)[-1]) {
-  for (i in 1:nrow(enrichComplex)) {
-    enrichComplex[i, j] = phyper(
-      nrow(wilcoxRes_adjusted_long[wilcoxRes_adjusted_long$complex == enrichComplex$complex[i] &
-                                     wilcoxRes_adjusted_long[, j] == T & wilcoxRes_adjusted_long$sign == T,]) - 1,
-      sum(phenotype[,j] == T),
-      sum(phenotype[,j] == F),
-      sum(wilcoxRes_adjusted_long$complex == enrichComplex$complex[i] & wilcoxRes_adjusted_long$sign == T),
-      lower.tail = F
-    )
-  }
+clusteringDF_selected = clusteringDF[clusteringDF$walktrapCluster %in% rownames(wilcoxRes_binary)[rowSums(wilcoxRes_binary) > 0],]
+
+d1 = clusteringDF_selected[,c("clust1", "clust2")]
+d1 = unique(d1)
+colnames(d1) = c('from', 'to')
+d2 = clusteringDF_selected[,c("clust2", "clust3")]
+d2 = unique(d2)
+colnames(d2) = c('from', 'to')
+d3 = clusteringDF_selected[,c("clust3", "clust4")]
+d3 = unique(d3)
+colnames(d3) = c('from', 'to')
+d4 = clusteringDF_selected[,c("clust4", "clust5")]
+d4 = unique(d4)
+colnames(d4) = c('from', 'to')
+d5 = clusteringDF_selected[,c("clust5", "clust6")]
+d5 = unique(d5)
+colnames(d5) = c('from', 'to')
+
+edges <- rbind(d1, d2, d3, d4, d5)
+mygraph <- graph_from_data_frame(edges)
+
+V(mygraph)$nGene = table(unlist(clusteringDF[,2:7]))[match(V(mygraph)$name, names(table(unlist(clusteringDF[,2:7]))))]
+V(mygraph)$ciliopathyCluster = V(mygraph)$name %in% rownames(wilcoxRes_binary)[rowSums(wilcoxRes_binary) > 0]
+
+ggraph(mygraph, layout = 'dendrogram', circular = FALSE) + 
+  geom_edge_link(arrow = arrow(length = unit(2, 'mm')), 
+                 end_cap = circle(3, 'mm')) +
+  geom_node_point(aes(size = nGene), color = '#93C1E8') +
+  theme_void() + geom_node_text(aes(label = name, color = ciliopathyCluster), size = 3) + scale_size_continuous(breaks = seq(1000,16000,2000))
+
+# annotate protein modules with GO terms ----
+
+PPIClustersCiliopathies = list()
+for (i in unique(rownames(wilcoxRes_binary)[rowSums(wilcoxRes_binary) > 0])){
+  PPIClustersCiliopathies[[i]] = enrichGO(PPIClusters[PPIClusters$walktrapCluster == i,"gene"], OrgDb = 'org.Hs.eg.db', keyType = 'ENSEMBL', ont = "BP")
 }
-
-#enrichComplex[,-1] = apply(enrichComplex[,-1], 2, function(x)p.adjust(x,method = 'BH'))
-enrichComplex[,-1] = -log10(enrichComplex[,-1])
-
-enrichComplex_sig = enrichComplex[apply(enrichComplex[,-1],1,max) > 1.3,]
-
-enrichComplex_sig = gather(enrichComplex_sig, key = 'phenotype', value = 'pvalue', -complex)
-
-ggplot(enrichComplex_sig,
-       aes(
-         x = phenotype,
-         y = complex,
-         size = pvalue,
-         color = pvalue
-       )) + geom_point() + theme_classic() + 
-  scale_size_continuous(range = c(-1, 10), breaks = seq(0.5, 4, 0.5)) + 
-  scale_color_gradient(low = 'white', high = 'red')
-
-
-
-
-
-
